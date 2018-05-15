816b683048f368bfa72f4e005072eabb4cba8de0
riyga <riyga25@gmail.com>
2018-05-16 01:03:11 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './sass/styles.scss';
+
+module.exports = {
+
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+        }]
+    }
+};
