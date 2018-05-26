f4899d0f49e794653614bb1ec46fc312b467d510
riyga <riyga@mail.ru>
2018-05-26 19:24:37 +0400

    change vue template

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,88 @@
 <template>
   <div id="app">
-        <header class="header">
-        <div class="wrapper header__container">
-            <a href="/" class="header__left">
-                Прогуляй стипендию!
-            </a>
-            <div class="header__center">
-              Размер стипендии <input type="text" class="h-money" value="2000"> ₽
-            </div>
-            <div class="header__right" @click="xx">
-                Добавить место
-            </div>
+    <header class="header">
+      <div class="wrapper header__container">
+        <a href="/" class="header__left">
+          Прогуляй стипендию!
+        </a>
+        <div class="header__center">
+          Размер стипендии <input type="text" class="h-money" value="2000"> ₽
         </div>
+        <div class="header__right" @click="plusOne">
+          Добавить место
+        </div>
+      </div>
     </header>
     <main>
-        <div class="preheader">
-            <div class="wrapper">
-                <a href="#" class="preheader__desk preheader__desk_back">Вернуться к списку</a>
-                <h2 class="perheader__title">Добавление места</h2>
-            </div>
+      <div class="preheader">
+        <div class="wrapper">
+          <a href="#" class="preheader__desk preheader__desk_back">Вернуться к списку</a>
+          <h2 class="perheader__title">Добавление места</h2>
         </div>
-        <div class="main-info">
-            <div class="wrapper flex-container">
-                <div class="inputs">
-                    <div class="inputs__list">
-                        <div class="inputs__item">
-                            <label for="place-name">Название</label>
-                            <input type="text" id="place-name" placeholder="Введите название места">
-                        </div>
-                        <div class="inputs__item">
-                            <label for="place-address">Адрес:</label>
-                            <input type="text" id="place-address" placeholder="Введите точный адрес места">
-                        </div>
-                        <div class="inputs__item">
-                            <label for="place-average">Средний чек:</label>
-                            <input type="text" id="place-average" placeholder="Введите размер среднего чека, руб.">
-                        </div>
-                        <div class="inputs__item">
-                            <label for="place-category">Категория:</label>
-                            <select id="place-category">
-                                <option value="">Бар</option>
-                            </select>
-                        </div>
-                        <div class="inputs__item upload-file">
-                            <label class="upload-file__label" for="place-photo">
-                                Фото:
-                                <span class="upload-file__place"></span>
-                                <input class="upload-file__input" type="file" id="place-photo" accept="image/jpeg,image/jpg,image/png">
-                            </label>
-                            <div class="upload-file__preview">
-                                <img src="./assets/img/uploadphoto.jpg" alt="image">
-                            </div>
-                        </div>
-                    </div>
+      </div>
+      <div class="main-info">
+        <div class="wrapper flex-container">
+          <div class="inputs">
+            <div class="inputs__list">
+              <div class="inputs__item">
+                <label for="place-name">Название</label>
+                <input type="text" id="place-name" placeholder="Введите название места">
+              </div>
+              <div class="inputs__item">
+                <label for="place-address">Адрес:</label>
+                <input type="text" id="place-address" placeholder="Введите точный адрес места">
+              </div>
+              <div class="inputs__item">
+                <label for="place-average">Средний чек:</label>
+                <input type="text" id="place-average" placeholder="Введите размер среднего чека, руб.">
+              </div>
+              <div class="inputs__item">
+                <label for="place-category">Категория:</label>
+                <select id="place-category">
+                  <option value="">Бар</option>
+                </select>
+              </div>
+              <div class="inputs__item upload-file">
+                <label class="upload-file__label" for="place-photo">
+                  Фото:
+                  <span class="upload-file__place"></span>
+                  <input class="upload-file__input" type="file" id="place-photo" accept="image/jpeg,image/jpg,image/png">
+                </label>
+                <div class="upload-file__preview">
+                  <img src="./assets/img/uploadphoto.jpg" alt="image">
                 </div>
-                <div class="big-map">
-                    <div class="big-map__label">Укажите место на карте:</div>
-                    <div class="big-map__img" style="background-image: url('./src/assets/img/bigmap.png');background-position: center"></div>
-                </div>
+              </div>
             </div>
+          </div>
+          <div class="big-map">
+            <div class="big-map__label">Укажите место на карте:</div>
+            <div class="big-map__img" style="background-image: url('../static/bigmap.png');background-position: center">
+
+            </div>
+          </div>
         </div>
+      </div>
     </main>
     <footer class="footer">
-        <div class="wrapper">
-            <p class="footer__copyright">SimborSoft</p>
-        </div>
+      <div class="wrapper">
+        <p class="footer__copyright">SimborSoft</p>
+      </div>
     </footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data: function () {
-    return{
-      count: 0
-    }
-  },
-  methods:{
-    xx: function () {
-      console.log(this.count++);
+  export default {
+    name: 'app',
+    data: function () {
+      return{
+        count: 0,
+      }
+    },
+    methods:{
+      plusOne : function () {
+        console.log(this.count++);
+      }
     }
   }
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App'
-import './assets/styles/styles.scss';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import '../src/styles/styles.scss';
 
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App),
+  components: { App },
+  template: '<App/>',
 });
